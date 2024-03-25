674be152d9f083f54d2ad9ef9bf92f7d7d48e84e
fbo1224 <161474075+fbo1224@users.noreply.github.com>
2024-03-25 15:01:01 +0900

    커커밋 - 동혁

--- Thenolja/src/thenolja/member/controller/MemberUpdateController.java
+++ Thenolja/src/thenolja/member/controller/MemberUpdateController.java
@@ -1,11 +1,16 @@
 package thenolja.member.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import thenolja.member.model.service.MemberService;
+import thenolja.member.model.vo.Member;
 
 /**
  * Servlet implementation class MemberUpdateController
@@ -27,7 +32,7 @@ public class MemberUpdateController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		/*
+		
 		request.setCharacterEncoding("UTF-8");
 		
 		String memName = request.getParameter("memName");
@@ -56,7 +61,7 @@ public class MemberUpdateController extends HttpServlet {
 		} else {
 			request.setAttribute("errorMsg", "정보수정에 실패하였습니다.");
 			request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
-		}*/
+		}
 		System.out.println("정보수정2 서블릿");
 		request.getRequestDispatcher("views\\member\\memberUpdate.jsp").forward(request, response);
 	
