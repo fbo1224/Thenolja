a76bd75b03ad2704f265d177a277822c277129bf
fbo1224 <161474075+fbo1224@users.noreply.github.com>
2024-03-28 15:50:38 +0900

    커커밋 비회원예약조회

--- Thenolja/src/thenolja/nonmem/dao/NonmemDao.java
+++ Thenolja/src/thenolja/nonmem/dao/NonmemDao.java
@@ -1,28 +1,39 @@
 package thenolja.nonmem.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import thenolja.admin.member.model.dao.MemberDao;
-import thenolja.member.model.vo.Member;
+import thenolja.common.JDBCTemplate;
 import thenolja.nonmem.model.vo.SelectNonmemReser;
 
 public class NonmemDao {
 	
 	private Properties prop = new Properties();
 	
-	public MemberDao() {
+	public NonmemDao() {
 		String sqlFile = MemberDao.class.getResource("/sql/nonmem/nonmem-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(sqlFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
-	public Member selectNonmemReser(Connection conn, String nonmemName, String nonmemPhone) {
+	public ArrayList<SelectNonmemReser> selectNonmemReser(Connection conn, String nonmemName, String nonmemPhone) {
 		
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		SelectNonmemReser nonmemReser = null;
+		ArrayList<SelectNonmemReser> list = new ArrayList();
+		
 		
 		String sql = prop.getProperty("selectNonmemReser");
 		
@@ -36,38 +47,31 @@ public class NonmemDao {
 			
 			while(rset.next()) {
 				
-				nonmemReser = new Member(rset.getInt("MEM_NO"),
-						                 rset.getString("MEM_NAME"),
-						                 rset.getString("MEM_PHONE"),
-						                 rset.getString("MEM_STATUS"));
-				
-
-				
-				
-				
-				
+				SelectNonmemReser nonmemReser = new SelectNonmemReser(rset.getInt("MEM_NO"),
+						                            rset.getString("MEM_NAME"),
+						                            rset.getString("MEM_PHONE"),
+						                            rset.getString("MEM_STATUS"),
+						                            rset.getInt("RESER_NO"),
+						                            rset.getString("RESER_NAME"),
+						                            rset.getString("CHECKIN_TIME"),
+						                            rset.getString("CHECKOUT_TIME"),
+								                 	rset.getString("BICYCLE"),
+								                 	rset.getInt("PEOPLE"),
+								                 	rset.getString("ROOM_NAME"),
+								                 	rset.getInt("ROOM_NUM"),
+								                 	rset.getInt("MAX_PEOPLE"),
+								                 	rset.getString("CHECKIN_TIME"),
+								                 	rset.getString("CHECKOUT_TIME"),
+								                 	rset.getString("ROOM_IMG"));
+				list.add(nonmemReser);
 			}
-			
-			
-			
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		return list;
 	}
 	
 	
