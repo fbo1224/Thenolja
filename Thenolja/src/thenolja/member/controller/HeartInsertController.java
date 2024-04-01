dd7129276ca4a23c36aea3db33cbd6db8ca1d1a6
fbo1224 <161474075+fbo1224@users.noreply.github.com>
2024-04-01 16:32:02 +0900

    찜하기555

--- Thenolja/src/thenolja/member/controller/HeartInsertController.java
+++ Thenolja/src/thenolja/member/controller/HeartInsertController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HeartInsertController
  */
-@WebServlet("/insert.heart")
+@WebServlet("/insertheart")
 public class HeartInsertController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
