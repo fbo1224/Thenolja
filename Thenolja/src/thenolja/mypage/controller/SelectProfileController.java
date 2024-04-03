7fb99cabf6fd36f313a2cceaad7300df7eca74ff
fbo1224 <161474075+fbo1224@users.noreply.github.com>
2024-04-03 16:14:28 +0900

    프사변경2

---
+++ Thenolja/src/thenolja/mypage/controller/SelectProfileController.java
@@ -0,0 +1,51 @@
+package thenolja.mypage.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import thenolja.mypage.service.MyPageService;
+
+/**
+ * Servlet implementation class SelectProfileController
+ */
+@WebServlet("/insertProfile")
+public class SelectProfileController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SelectProfileController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int memNo = Integer.parseInt(request.getParameter("memNo"));
+		
+		String filePath = new MyPageService().selectProfile(memNo);
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		response.getWriter().print(filePath);
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
