7b4bf2200451124afb530d01cae1cce16529b853
JeongBin <happyjb11@naver.com>
2024-04-09 14:25:19 +0900

    건희님 ㄳ해여

--- Thenolja/src/thenolja/admin/reservation/controller/ReserMemberController.java
+++ Thenolja/src/thenolja/admin/reservation/controller/ReserMemberController.java
@@ -1,7 +1,10 @@
 package thenolja.admin.reservation.controller;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -69,7 +72,19 @@ public class ReserMemberController extends HttpServlet {
 		//  System.out.println(pi);
 		
 		ArrayList<AdminReservation> list = new ReservatoinService().selectReserMember(pi);
-		
+	    SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy.MM.dd");
+	    Date current = new Date();
+	    
+	    for(int i = 0; i < list.size(); i++) {
+        	try {
+				boolean reserStatus = simpleDate.parse(list.get(i).getCheckInTime()).before(current);
+				System.out.println(reserStatus);
+				list.get(i).setReserStatus(reserStatus);
+        	} catch (ParseException e) {
+				e.printStackTrace();
+			}
+	    }
+	    
 		request.setAttribute("selectReserMember", list);
 		request.setAttribute("pageInfo", pi);
 		// System.out.println(list);
